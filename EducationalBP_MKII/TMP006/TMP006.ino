82e55e18b01e6cfd001317ab5ea76aec00da2a91
A0271518 <a0271518@ti.com>
2013-12-04 10:04:21 -0600

    [ADDED}] Several sketches. TMP006 still in work This is an initial commit for the repo

---
+++ EducationalBP_MKII/TMP006/TMP006.ino
@@ -0,0 +1,26 @@
+
+/*
+ Infrared temp sensor TMP006 example for Educational BoosterPack MK II
+ http://boosterpackdepot.info/wiki/index.php?title=Educational_BoosterPack_MK_II
+ 
+
+Modified 03 Dec 2013
+by Dung Dang
+
+This example code is in the public domain.
+*/
+
+#include <Tmp006.h>
+
+void setup()
+{ // Initalizes the TMP006 for operation and for I2C communication
+  TMP006_Init(Eight);  // Takes 8 averaged samples for measurement
+}
+
+void loop()
+{
+  //Prints the temperature 
+  Print_Temp(Enable); // Enable: Prints temperature (c) and raw register values
+                      // Disable: Prints just the temperature (c)
+  
+}
